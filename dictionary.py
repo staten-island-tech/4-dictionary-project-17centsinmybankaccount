bdbd729596a1999eaeb219b2b50dd887d39dfa20
17centsinmybankaccount <ansonc43@nycstudents.net>
2026-02-06 10:13:59 -0500

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,16 @@
 thisdict = {
-    "egg":"kidney,5000,rotting"
-    "potato":"potato,10,rotting"
-    "mouse":"mouse,1,possibly edible"
-}
+    "egg" : ["kidney,5000,rotting"],
+    "potato" : ["potato,10,rotting"],
+    "mouse" : ["mouse,1,possibly edible"] }
+y = []
+while True:
+    action = input("What do you want to do")
+    if action == "shop":
+        x = input("We have egg, potato, and mouse")
+        print(thisdict[x])
+        y.append(x)
+    if action == "see cart":
+        print(y)
+    if action == "end":
+        print(y)
+        break
